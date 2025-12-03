998d60620c258580dac22306b4c33e7cce515e13
Julien Thibeault <julienthibeault@example.com>
2025-12-03 17:00:59 -0600

    Restore next-intl plugin without path argument

--- next.config.js
+++ next.config.js
@@ -1,3 +1,7 @@
+const createNextIntlPlugin = require('next-intl/plugin');
+
+const withNextIntl = createNextIntlPlugin();
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
@@ -27,4 +31,4 @@ const nextConfig = {
   },
 };
 
-module.exports = nextConfig;
+module.exports = withNextIntl(nextConfig);
