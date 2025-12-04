d57c081144a4ac72faf1beb8ed457ff13296498e
Julien Thibeault <julienthibeault@example.com>
2025-12-04 14:50:48 -0600

    Center journey steps grid with 3 columns

--- app/[locale]/welcome-pack/page.tsx
+++ app/[locale]/welcome-pack/page.tsx
@@ -329,7 +329,7 @@ export default function WelcomePackPage() {
             </p>
           </div>
 
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-6 max-w-4xl mx-auto">
             {journeySteps.map((step, index) => (
               <div key={index} className="relative">
                 {/* Connector line */}
