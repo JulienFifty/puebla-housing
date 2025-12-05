ee046bf8a3f9cf5fbdd6ec8eddbe1a894e872c42
Julien Thibeault <julienthibeault@example.com>
2025-12-05 14:03:20 -0600

    Slightly increase logo size (h-5/h-6/h-8)

--- components/Header.tsx
+++ components/Header.tsx
@@ -69,7 +69,7 @@ export default function Header() {
               alt="Puebla Housing"
               width={140}
               height={45}
-              className="h-4 sm:h-5 md:h-7 w-auto transition-transform group-hover:scale-105 drop-shadow-lg"
+              className="h-5 sm:h-6 md:h-8 w-auto transition-transform group-hover:scale-105 drop-shadow-lg"
               priority
             />
           </Link>
